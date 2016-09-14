97a99da8e76bc85d3b7d5f8ec5eeda8bc148761a
aM3z <amezolma@plu.edu>
2016-09-13 20:38:41 -0700

    start implementing Neuron.ts

---
+++ mentis/src/Neuron.ts
@@ -0,0 +1,24 @@
+class Neuron {
+
+	private weights: number[]; 
+	private bias: number; 
+	private learningRate: number;
+	
+	constructor() {}
+	
+	public predict(inputs: number[]): number { 
+		return 0; 
+	}
+
+	private SGD(): void {}
+
+	private randomWeights():void {}
+
+	private sigmoidFunction(): number {
+		return 0;
+	}
+
+	private dotProduct(a: number[], b: number[]): number { 
+		return 0; 
+	}
+}
