44cfd3fcda29be7b888af2c49f4a515f25d826ca
aM3z <amezolma@plu.edu>
2016-09-25 21:06:28 -0700

    add licensing information

--- src/cafa/SequenceSummary.java
+++ src/cafa/SequenceSummary.java
@@ -27,36 +27,6 @@ package cafa;
  *
  * @author Miguel Amezola <amezolma@plu.edu>
  */
-public class Protein {
-    // UniProtKB Accession
-    private String accession;
-    // Sequence
-    private String sequence;
-
-    public Protein(String accession, String sequence) {
-        this.accession = accession;
-        this.sequence = sequence;
-    }
-
-    public String getAccession() {
-        return accession;
-    }
-
-    public void setAccession(String accession) {
-        this.accession = accession;
-    }
-
-    public String getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(String sequence) {
-        this.sequence = sequence;
-    }
-
-    @Override
-    public String toString() {
-        return ">" + accession + "\n" + sequence + "\n";
-    }
+public class SequenceSummary {
     
 }
